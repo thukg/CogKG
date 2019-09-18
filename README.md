684c96028bbd2150e9285e2196640bd8919af4fd
luogan <luogan_62625686@126.com>
2019-09-18 09:53:48 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # CogKG
 CogKG: A Versatile Toolkit Collection on Knowledge Graph
+
+## usage
+
+Use `bash download_toolkits.sh` to download all toolkits in CogKG. (Clone with HTTPS)
---
+++ download_toolkits.sh
@@ -0,0 +1,6 @@
+git clone https://github.com/thukg/link_prediction.git
+git clone https://github.com/thukg/query-entity-recognition.git
+git clone https://github.com/thukg/FastKATE.git
+git clone https://github.com/thukg/query-intent-classification.git
+git clone https://github.com/thukg/concept-expansion-snippet.git
+git clone https://github.com/thukg/concept-expansion-kg.git
